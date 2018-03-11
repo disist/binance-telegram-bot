e72591cd3b9ba1728deefdee144db46e662635a4
Dmytro Golysh <disist357@gmail.com>
2018-03-11 21:16:38 +0200

    feat(deploy): fix port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 console.log('PORT is', port);
 const requestHandler = (request, response) => {
     response.end('The bot is running, everything is ok!');
 }
 const server = http.createServer(requestHandler);
-server.listen(port, (err) => {
+server.listen(port,  '0.0.0.0', (err) => {
     if (err) {
         return console.log('something bad happened', err);
     }
